3f8a5a4bfe7fa6354ada6cfa08ae609d7be5b2fe
Reinhold <reinhold@bentele.org>
2018-05-24 21:44:30 +0200

    Add a controller

---
+++ src/main/java/org/bentele/demo/HomeController.java
@@ -0,0 +1,14 @@
+package org.bentele.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	public String hello() {
+		return "hello";
+	}
+
+}
