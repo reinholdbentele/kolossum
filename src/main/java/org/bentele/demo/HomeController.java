8f6cb1732feb85cd5e0e94e2dde8b0f7e73df74b
Reinhold <reinhold@bentele.org>
2018-05-24 22:21:56 +0200

    Add a parameter to the request

--- src/main/java/org/bentele/demo/HomeController.java
+++ src/main/java/org/bentele/demo/HomeController.java
@@ -1,14 +1,15 @@
 package org.bentele.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
 	@RequestMapping("/")
-	public String hello() {
-		return "hello";
+	public String hello(@RequestParam(required = false) final String in) {
+		return "hello " + in;
 	}
 
 }
